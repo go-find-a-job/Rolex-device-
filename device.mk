28d36646130c912a0541b803cf7d75e22755cf0d
muralivijay <muralidharan9845@gmail.com>
2019-05-23 16:15:55 +0000

    update radio bloob

--- device.mk
+++ device.mk
@@ -304,6 +304,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0
 
+# Telephony
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
